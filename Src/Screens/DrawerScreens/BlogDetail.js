dff589d9acee1bb9e40494685a1b65921ae6928e
Emre Özer <eozer1371@gmail.com>
2025-05-24 21:35:00 +0300

    Blog page has been completed.

---
+++ Src/Screens/DrawerScreens/BlogDetail.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function BlogDetail() {
+  return (
+    <View>
+      <Text>BlogDetail</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
